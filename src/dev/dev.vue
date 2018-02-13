58e2d51ddf8b59f2773bf8792abaea6de73c751a
HKCLY <2460102575@qq.com>
2018-02-14 02:13:57 +0800

    dev上第一次提交

---
+++ src/dev/dev.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      hello -dev
+  </div>
+</template>
+ <script>
+ export default {
+   data () {
+       return {
+           devMessage: "hello -dev"
+       }
+   }
+ }
+ </script>
+ 
